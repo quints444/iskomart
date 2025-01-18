c314b9418f30258c4645b0c6fd4342ba3f2a579a
quints444 <quintanamarclorenz4@gmail.com>
2025-01-19 00:09:38 +0800

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Angela, JM, Marc</Text>
       <StatusBar style="auto" />
     </View>
   );
